15a6e43d09249c9f09915f0d53e05be0ef4499c7
Antonio Marinov <antonio.marinovvvv@gmail.com>
2014-10-30 13:58:11 -0700

    first commit

---
+++ Flappy Bird Pro iOS APP/Flappy Bird Pro iOS APP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flappy Bird Pro iOS APP
+//
+//  Created by Antonio on 10/30/14.
+//  Copyright (c) 2014 Antonio Marinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flappy Bird Pro iOS APP/Flappy Bird Pro iOS APP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Flappy Bird Pro iOS APP
+//
+//  Created by Antonio on 10/30/14.
+//  Copyright (c) 2014 Antonio Marinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
